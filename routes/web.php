f69cd796cc3827f26e13060ab36bd4d24640dfb9
clairenjuguna <109039274+clairenjuguna@users.noreply.github.com>
2024-01-11 04:42:39 +0300

    frontend complete

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get("/home",[HomeController::class,"index"]);
 
 require __DIR__.'/auth.php';

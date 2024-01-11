74b91d74483b6e7d7740fda16fdb6867a5b48c6c
clairenjuguna <109039274+clairenjuguna@users.noreply.github.com>
2024-01-11 13:16:33 +0300

    admin side set up inittially

---
+++ resources/views/admin/navbar.blade.php
@@ -0,0 +1,46 @@
+<li class="nav-item nav-category">
+    <span class="nav-link">Navigation</span>
+  </li>
+  <li class="nav-item menu-items">
+    <a class="nav-link" href="index.html">
+      <span class="menu-icon">
+        <i class="mdi mdi-speedometer"></i>
+      </span>
+      <span class="menu-title">Users</span>
+    </a>
+  </li>
+  <li class="nav-item menu-items">
+    <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+      <span class="menu-icon">
+        <i class="mdi mdi-laptop"></i>
+      </span>
+      <span class="menu-title">Foods</span>
+      <i class="menu-arrow"></i>
+    </a>
+    <div class="collapse" id="ui-basic">
+      <ul class="nav flex-column sub-menu">
+        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
+        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
+        <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
+      </ul>
+    </div>
+  </li>
+  <li class="nav-item menu-items">
+    <a class="nav-link" href="pages/forms/basic_elements.html">
+      <span class="menu-icon">
+        <i class="mdi mdi-playlist-play"></i>
+      </span>
+      <span class="menu-title">Chefs</span>
+    </a>
+  </li>
+  <li class="nav-item menu-items">
+    <a class="nav-link" href="pages/tables/basic-table.html">
+      <span class="menu-icon">
+        <i class="mdi mdi-table-large"></i>
+      </span>
+      <span class="menu-title">Reservations</span>
+    </a>
+  </li>
+
+
+  </li>
